73f5edd2d7de0beda40f9cd40155fdf2cef7f70f
Alex Le <alex@wordnik.com>
2012-05-05 13:53:43 -0700

    - intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZuckJs::Application.config.secret_token = '9fb29e6443286de837a3904ea13edd0b82b8f89991cf856719c9ec462d042661df30cacc85b527de023d1473c9b67ebc87c6c98d938d8e6a3289dc67a54a7fec'
